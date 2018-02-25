7f6e097efe69e3b4175c4ac8ebaaed844c3e27c5
Michael Santos <michael.santos@gmail.com>
2018-02-25 10:17:22 -0500

    runlimit: restart limits for supervised processes
    runlimit is a POC for implementing restart intensity limits for possibly
    unrelated processes running under a supervisor such as daemontools'
    svscan(8).

---
+++ Makefile
@@ -0,0 +1,47 @@
+.PHONY: all clean test
+
+PROG=   runlimit
+SRCS=   runlimit.c \
+        strtonum.c
+
+UNAME_SYS := $(shell uname -s)
+ifeq ($(UNAME_SYS), Linux)
+    CFLAGS ?= -D_FORTIFY_SOURCE=2 -O2 -fstack-protector-strong \
+              -Wformat -Werror=format-security \
+              -fno-strict-aliasing
+	  RUNLIMIT_SANDBOX ?= seccomp
+else ifeq ($(UNAME_SYS), OpenBSD)
+    CFLAGS ?= -DHAVE_STRTONUM \
+              -D_FORTIFY_SOURCE=2 -O2 -fstack-protector-strong \
+              -Wformat -Werror=format-security \
+              -fno-strict-aliasing
+	  RUNLIMIT_SANDBOX ?= pledge
+else ifeq ($(UNAME_SYS), FreeBSD)
+    CFLAGS ?= -DHAVE_STRTONUM \
+              -D_FORTIFY_SOURCE=2 -O2 -fstack-protector-strong \
+              -Wformat -Werror=format-security \
+              -fno-strict-aliasing
+	  RUNLIMIT_SANDBOX ?= capsicum
+endif
+
+RM ?= rm
+
+RUNLIMIT_SANDBOX ?= rlimit
+RUNLIMIT_CFLAGS ?= -g -Wall -fwrapv
+
+CFLAGS += $(RUNLIMIT_CFLAGS) \
+          -DRUNLIMIT_SANDBOX=\"$(RUNLIMIT_SANDBOX)\" \
+          -DRUNLIMIT_SANDBOX_$(RUNLIMIT_SANDBOX)
+
+LDFLAGS += $(RUNLIMIT_LDFLAGS) -lrt -Wl,-z,relro,-z,now
+
+all: $(PROG)
+
+$(PROG):
+	$(CC) $(CFLAGS) -o $(PROG) $(SRCS) $(LDFLAGS)
+
+clean:
+	-@$(RM) $(PROG)
+
+test: $(PROG)
+	@PATH=.:$(PATH) bats test
---
+++ runlimit.c
@@ -0,0 +1,264 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <err.h>
+#include <getopt.h>
+#include <time.h>
+#include <string.h>
+#include <sys/types.h>
+
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#include <signal.h>
+
+#include <errno.h>
+
+#include <limits.h>
+
+#include "runlimit.h"
+
+#define RUNLIMIT_VERSION "0.1.0"
+
+#ifdef CLOCK_MONOTONIC_COARSE
+#define RUNLIMIT_CLOCK_MONOTONIC CLOCK_MONOTONIC_COARSE
+#else
+#define RUNLIMIT_CLOCK_MONOTONIC CLOCK_MONOTONIC
+#endif
+
+#define VERBOSE(__n, ...) do { \
+    if (verbose >= __n) { \
+        (void)fprintf(stderr, __VA_ARGS__); \
+    } \
+} while (0)
+
+#define EXIT_ERRNO (127+errno)
+
+extern char *__progname;
+
+typedef struct {
+  u_int32_t intensity;
+  struct timespec now;
+} runlimit_t;
+
+enum {
+  OPT_DRYRUN = 1,
+  OPT_SIGNAL = 2,
+  OPT_WAIT   = 4,
+  OPT_PRINT  = 8,
+};
+
+static const struct option long_options[] =
+{
+  {"intensity",   required_argument,  NULL, 'i'},
+  {"period",      required_argument,  NULL, 'p'},
+  {"signal",      required_argument,  NULL, 's'},
+  {"dryrun",      no_argument,        NULL, 'n'},
+  {"print",       no_argument,        NULL, 'P'},
+  {"verbose",     no_argument,        NULL, 'v'},
+  {"wait",        no_argument,        NULL, 'w'},
+  {"help",        no_argument,        NULL, 'h'},
+  {NULL,          0,                  NULL, 0}
+};
+
+static int state_open(char *name);
+static int state_create(char *name);
+static void usage();
+
+  int
+main(int argc, char *argv[])
+{
+  int fd;
+  runlimit_t *ap;
+  char name[NAME_MAX-1] = {0};
+  u_int32_t intensity = 1;
+  int period = 1;
+  struct timespec now;
+  int diff;
+  int remaining;
+  int opt = 0;
+  int verbose = 0;
+  int ch;
+  int n = 0;
+  int rv = 0;
+  int sig = 0;
+
+  /* initialize local time before entering sandbox */
+  if (clock_gettime(RUNLIMIT_CLOCK_MONOTONIC, &now) < 0)
+    err(EXIT_ERRNO, "clock_gettime(CLOCK_MONOTONIC)");
+
+  while ((ch = getopt_long(argc, argv, "hi:nPp:s:vw", long_options, NULL)) != -1) {
+    switch (ch) {
+      case 'i':
+        intensity = strtonum(optarg, 1, UINT_MAX, NULL);
+        if (errno)
+          err(EXIT_FAILURE, "strtonum: %s", optarg);
+        break;
+
+      case 'n':
+        opt |= OPT_DRYRUN;
+        break;
+
+      case 'P':
+        opt |= OPT_PRINT;
+        break;
+
+      case 'p':
+        period = strtonum(optarg, 1, INT_MAX, NULL);
+        if (errno)
+          err(EXIT_FAILURE, "strtonum: %s", optarg);
+        break;
+
+      case 's':
+        sig = strtonum(optarg, 1, NSIG, NULL);
+        if (errno)
+          err(EXIT_FAILURE, "strtonum: %s", optarg);
+        break;
+
+      case 'v':
+        verbose++;
+        break;
+
+      case 'w':
+        opt |= OPT_WAIT;
+        break;
+
+      case 'h':
+      default:
+        usage();
+    }
+  }
+
+  if (argc - optind == 0)
+    usage();
+
+  n = snprintf(name, sizeof(name), "/%s-%d-%s", __progname, getuid(),
+      argv[optind]);
+
+  if (n < 0 || n >= sizeof(name))
+    usage();
+
+  fd = state_open(name);
+
+  if (fd < 0)
+    err(EXIT_ERRNO, "state_open");
+
+  ap = mmap(NULL, sizeof(runlimit_t), PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
+
+  if (ap == MAP_FAILED)
+    err(EXIT_ERRNO, "mmap");
+
+  diff = now.tv_sec - ap->now.tv_sec;
+  if (diff < 0)
+    diff = 0;
+
+  VERBOSE(1,
+      "now=%lu\n"
+      "last=%lu\n"
+      "diff=%u\n"
+      "intensity=%u\n"
+      "period=%d\n"
+      "count=%u\n",
+      now.tv_sec,
+      ap->now.tv_sec,
+      diff,
+      intensity,
+      period,
+      ap->intensity);
+
+  remaining = period <= diff ? 0 : period - diff;
+
+  if (opt & OPT_PRINT)
+    (void)printf("%d\n", remaining);
+
+  switch (remaining) {
+    case 0:
+      ap->intensity = 1;
+      ap->now.tv_sec = now.tv_sec;
+      ap->now.tv_nsec = 0;
+      break;
+
+    default:
+      if (ap->intensity >= intensity) {
+        VERBOSE(1, "error: threshold reached\n");
+        if (opt & OPT_WAIT) {
+          int sleepfor = remaining;
+          while (sleepfor > 0)
+            sleepfor = sleep(sleepfor);
+          if (execvp(argv[0], argv) < 0)
+            err(EXIT_ERRNO, "execve");
+        }
+        rv = 111;
+      }
+      else {
+        ap->intensity++;
+      }
+      break;
+  }
+
+  if (msync(ap, sizeof(runlimit_t), MS_SYNC|MS_INVALIDATE) < 0)
+    err(EXIT_ERRNO, "msync");
+
+  if (sig > 0 && rv == 111) {
+    VERBOSE(1, "sending signal %d to process group\n", sig);
+
+    if (!(opt & OPT_DRYRUN))
+      (void)kill(0, sig);
+  }
+
+  return rv;
+}
+
+  static int
+state_open(char *name)
+{
+  int fd;
+
+  fd = shm_open(name, O_RDWR, 0);
+
+  if (fd > -1)
+    return fd;
+
+  if (errno == ENOENT)
+    return state_create(name);
+
+  return -1;
+}
+
+  static int
+state_create(char *name)
+{
+  int fd;
+
+  fd = shm_open(name, O_RDWR|O_CREAT|O_EXCL, 0600);
+
+  if (fd < 0)
+    return -1;
+
+  if (ftruncate(fd, sizeof(runlimit_t)) < 0) {
+    int oerrno = errno;
+    (void)close(fd);
+    (void)shm_unlink(name);
+    errno = oerrno;
+    return -1;
+  }
+
+  return fd;
+}
+
+  static void
+usage()
+{
+  errx(EXIT_FAILURE, "[OPTION] <TAG>\n"
+    "version: %s (using %s sandbox)\n\n"
+    "-i, --intensity <count>  number of restarts\n"
+    "-p, --period <seconds>   time period\n"
+    "-s, --signal <signal>    send signal to process group\n"
+    "-w, --wait               wait until period expires\n"
+    "-n, --dryrun             do nothing\n"
+    "-P, --print              print remaining time\n"
+    "-v, --verbose            verbose mode\n",
+    RUNLIMIT_VERSION,
+    RUNLIMIT_SANDBOX);
+}
